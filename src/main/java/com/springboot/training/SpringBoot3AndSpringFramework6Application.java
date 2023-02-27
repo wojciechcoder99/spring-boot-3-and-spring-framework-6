9668725f0d962b864ba9980af969a56331671088
wojciechcoder99 <wojciech.farganus@capgemini.com>
2023-02-27 21:58:03 +0100

    init commit

---
+++ src/main/java/com/springboot/training/SpringBoot3AndSpringFramework6Application.java
@@ -0,0 +1,13 @@
+package com.springboot.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3AndSpringFramework6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3AndSpringFramework6Application.class, args);
+    }
+
+}
